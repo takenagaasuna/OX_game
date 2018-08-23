e2b0d6d461479b187e42046f349abcef73b27a83
takenagaasuna <atake0847@gmail.com>
2018-08-23 02:26:16 +0000

    game仮完成

--- OX_game.rb
+++ OX_game.rb
@@ -1,48 +1,76 @@
-$board = Array.new(3,0)
-$board.length.times{ |i|
-  $board[i] = Array.new(3,0)
-}
-turn = 1
-$ox = 0
+def show
+  for num in 0..4 do
+    if num%2 == 0 then
+      for num2 in 0..2 do
+        case $board[num/2][num2]
+          when 0 then
+            ox = "  "
+          when 1 then
+            ox = "〇"
+          when 2 then
+            ox = "✕"
+        end
+        print(ox," | ")
+      end
+      print("\n")
+    else
+      13.times{
+        print("―")
+      }
+      print("\n")
+    end
+  end
+end
+  
 
-9.times{
-    for num in 0..4 do
-      if num%2 == 0 then
-          for num2 in 0..2 do
-              case $board[num/2][num2]
-                when 0 then
-                  ox = "△"
-                when 1 then
-                  ox = "〇"
-                when 2 then
-                  ox = "✕"
-              end
-            print(ox," | ")
-          end
-        print("\n")
-      else
-          13.times{
-            print("―")
-          }
-          print("\n")
-      end
+def win
+  for num in 0..2 do
+    if ($board[0][num] == $board[1][num] && $board[0][num] == $board[2][num] &&
+        $board[0][num]!=0)||
+       ($board[num][0..2].uniq.count == 1 && $board[num][0]!=0)||
+       ($board[0][0] == $board[1][1] && $board[0][0] == $board[2][2]) ||
+       ($board[0][2] == $board[1][1] && $board[0][2] == $board[2][0])
+        then
+      return true
     end
+  end
+  return false
+end
+
+$board = Array.new(3,0){ Array.new(3,0) }
+turn = 1
+
+for count in 0..8 do
+  print("\nPlayer",turn ," turn !\n\n")
+  show
     
-    print("input row-number: ")
-    row = gets.to_i
-    #p row
-    print("input column-number: ")
-    column = gets.to_i
-    #p column
-      
-    if $board[row][column]!=0 then
-       print("Cannot draw !!\n")
-    end
-      $board[row][column] = turn
-      #p $board
-      if turn == 1 then
-        turn += 1
-      else 
-        turn -= 1
-      end
-}
+  begin
+  print("input row-number: ")
+  row = gets.to_i
+  print("input column-number: ")
+  column = gets.to_i
+    
+  if $board[row][column] != 0 then 
+     print("Cannot draw !!\n")
+  end
+  end while $board[row][column] != 0
+
+  $board[row][column] = turn
+  judge = win
+  if judge == true then
+    show
+    print( "winner is player",turn,"!!\n")
+    break
+  end
+        
+  if turn == 1 then
+    turn += 1
+  else 
+    turn -= 1
+  end
+end
+
+if count == 8 then
+  show
+  print( "Draw !!\n")
+end
