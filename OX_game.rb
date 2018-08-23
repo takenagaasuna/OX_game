e2fa87bb651ec5af360464af6c45738f8e7e040c
takenagaasuna <atake0847@gmail.com>
2018-08-23 07:19:50 +0000

    game_showメソッド分割,

--- OX_game.rb
+++ OX_game.rb
@@ -1,18 +1,22 @@
-def show
+def draw(board,locate)
+  (0..2).each do |locate2 |
+    case board[locate/2][locate2]
+    when 0
+      ox = "  "
+    when 1
+      ox = "〇"
+    when 2
+      ox = "✕"
+    end
+    print(ox," | ")
+  end
+  print("\n")
+end
+
+def show(board)
   (0..4).each do | locate |
     if locate.even?
-      (0..2).each do |locate2 |
-        case $board[locate/2][locate2]
-        when 0
-          ox = "  "
-        when 1
-          ox = "〇"
-        when 2
-          ox = "✕"
-        end
-        print(ox," | ")
-      end
-      print("\n")
+      draw(board,locate)
     else
       13.times{ print("―") }
       print("\n")
@@ -20,62 +24,65 @@ def show
   end
 end
   
-def row_line(bingo)
-  if $board[bingo][0..2].uniq.count == 1 &&
-     $board[bingo][0]!=0 then
+def row_line(bingo,board)
+  if board[bingo][0..2].uniq.count == 1 &&
+     board[bingo][0]!=0
     true
   else
     false
   end
 end
 
-def column_line(bingo)
-  if $board[0][bingo] == $board[1][bingo] &&
-     $board[0][bingo] == $board[2][bingo] &&
-     $board[0][bingo]!=0 then
+def column_line(bingo,board)
+  if board[0][bingo] == board[1][bingo] &&
+     board[0][bingo] == board[2][bingo] &&
+     board[0][bingo]!=0
      true
   else
     false
   end
 end
 
-def diagonal_left
-  if $board[0][0] == $board[1][1] &&
-    $board[0][0] == $board[2][2] &&
-    $board[0][0]!=0 then
+def diagonal_left(board)
+  if board[0][0] == board[1][1] &&
+    board[0][0] == board[2][2] &&
+    board[0][0]!=0
     true
   else
     false
   end
 end
 
-def diagonal_right
-  if $board[0][2] == $board[1][1] &&
-      $board[0][2] == $board[2][0] &&
-      $board[0][2]!=0 then
+def diagonal_right(board)
+  if board[0][2] == board[1][1] &&
+      board[0][2] == board[2][0] &&
+      board[0][2]!=0
       true
     else
       false
   end
 end
 
-     
-def win
+def win(board)
+  flag = false
   (0..2).each do | bingo |
-    if row_line(bingo) || column_line(bingo) ||
-       diagonal_left || diagonal_right then
-      return true
+    if row_line(bingo,board) || column_line(bingo,board) ||
+       diagonal_left(board) || diagonal_right(board)
+      flag = true
     end
   end
-  return false
+  if flag
+    true
+  end
 end
 
-$board = Array.new(3,0){ Array.new(3,0) }
+board = Array.new(3,0){ Array.new(3,0) }
 turn = 1
+count =0
 
-(0..8).each do | i |
+(0..8).each do | cycle |
   print("\nPlayer",turn ," turn !\n\n")
-  show
+  show(board)
     
   begin
     print("input row-number(0~2): ")
@@ -83,28 +90,28 @@ turn = 1
     print("input column-number(0~2): ")
     column = gets.to_i
     
-    if row > 2|| column >2 then
+    if row > 2|| column >2
        print("Cannot draw !!\n")
        redo
     end
-    if $board[row][column] != 0 then
+    if board[row][column] != 0
        print("Cannot draw !!\n")
     end
-  end while $board[row][column] != 0
+  end while board[row][column] != 0
 
-  $board[row][column] = turn
-  judge = win
-  if judge then
-    show
-    print( "winner is player",turn,"!!\n")
+  board[row][column] = turn
+  judge = win(board)
+  if judge
+    show(board)
+    print( "winner is player",turn," !!\n")
     break
   end
   
-  @count = i
+  count = cycle
   turn == 1 ? turn += 1 : turn -= 1
 end
 
-if @count == 8
-  show
+if count == 8
+  show(board)
   print( "Draw !!\n")
 end
